26db9595a0207a8ccc0eaaa27712b96d055dcede
pekshevds <pekshev.ds@gmail.com>
2023-07-25 20:30:37 +0300

    add category & good models, left menu

--- django_web_store/catalog_app/admin.py
+++ django_web_store/catalog_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog_app.models import Category
+from catalog_app.models import Good
+
+
+class GoodAdmin(admin.ModelAdmin):
+    pass
+
+
+class InlineGoodlAdmin(admin.StackedInline):
+    model = Good
+
+
+class CatergryAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'parent', )
+    list_filter = ('parent', )
+    inlines = (InlineGoodlAdmin, )
+
+
+admin.site.register(Category, CatergryAdmin)
+admin.site.register(Good, GoodAdmin)
--- django_web_store/catalog_app/models.py
+++ django_web_store/catalog_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(verbose_name="Category", max_length=150, primary_key=True)
+    parent = models.ForeignKey("Category", related_name="childs", blank=True, null=True, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Good(models.Model):
+    name = models.CharField(verbose_name="Good", max_length=150, primary_key=True)
+    category = models.ForeignKey("Category", related_name="goods", blank=True, null=True, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.name
