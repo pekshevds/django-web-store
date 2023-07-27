94455f9b302853f66fa0da0288305f7e01802047
pekshevds <pekshev.ds@gmail.com>
2023-07-27 15:45:25 +0300

    add filling left menu

--- django_web_store/catalog_app/models.py
+++ django_web_store/catalog_app/models.py
@@ -3,10 +3,18 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(verbose_name="Category", max_length=150, primary_key=True)
-    parent = models.ForeignKey("Category", related_name="childs", blank=True, null=True, on_delete=models.PROTECT)
+    parent = models.ForeignKey("Category", related_name="childs1", blank=True, null=True, on_delete=models.PROTECT)
 
     def __str__(self) -> str:
         return self.name
+
+    @classmethod
+    def fetch_top_level_categories(cls):
+        return cls.objects.filter(parent=None)
+
+    @property
+    def childs(self):
+        return Category.objects.filter(parent=self)
 
 
 class Good(models.Model):
--- django_web_store/index_app/views.py
+++ django_web_store/index_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from catalog_app.models import Category
 
 
 def index(request):
 
     context = {
         'items': range(5000),
+        'categories': Category.fetch_top_level_categories()
     }
     return render(request, template_name="index_app/index.html", context=context)
